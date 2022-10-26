97e4ae7a8043193102d82e830d4de582cbe7ddd5
ola20sarumi <61728654+ola20sarumi@users.noreply.github.com>
2022-10-26 11:08:58 +0300

    initial commit

---
+++ WebApplication1200/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1200.Models;
+
+namespace WebApplication1200.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication1200.Models.Joke> Joke { get; set; }
+    }
+}
